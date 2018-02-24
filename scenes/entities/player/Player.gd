d59a74cb7af5aba86226d84becd83c204ab905ff
Benzcker <benz.checker@googlemail.com>
2018-02-24 07:16:02 +0100

    Add basic player movement

--- scenes/entities/player/Player.gd
+++ scenes/entities/player/Player.gd
@@ -1,14 +1,40 @@
 extends Area2D
+signal hit
 
-export (int) var SPEED = 400
+export (int) var SPEED = 200
 var velocity = Vector2()
+onready var screensize = get_viewport_rect().size
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+#func start(pos):
+#	position = pos
+
+func _process(delta):
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		# $AnimatedSprite.flip_v = velocity.y > 0
+
