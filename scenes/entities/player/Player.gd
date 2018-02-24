e35a498846ab6c2cbfca09e93ac08b0152d89339
Benzcker <benz.checker@googlemail.com>
2018-02-24 06:43:59 +0100

    First steps to player animation and collision

---
+++ scenes/entities/player/Player.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+export (int) var SPEED = 400
+var velocity = Vector2()
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
