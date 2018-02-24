2c6cb0123b25da30e4074a8824ae3c3ae8c5a0e0
Benzcker <benz.checker@googlemail.com>
2018-02-24 14:50:35 +0100

    Add player health and dash

--- scenes/entities/player/Player.gd
+++ scenes/entities/player/Player.gd
@@ -1,18 +1,43 @@
-extends Area2D
+extends RigidBody2D
 signal hit
 
-export (int) var SPEED = 200
+# Movement
+export (int) var SPEED = 1000
 var velocity = Vector2()
-onready var screensize = get_viewport_rect().size
+# onready var screensize = get_viewport_rect().size
+
+# Health
+export (int) var MAXHEALTH = 100
+export (int) var health = 100
+
+const empty_Vec2 = Vector2()
+
+# Attack
+var till_next_attack = 0
+var is_attacking = false
+export (float) var attack_duration = 0.05
+export (float) var attack_timeout = 1
 
 func _ready():
-	pass
+	linear_damp = 6
 
-#func start(pos):
-#	position = pos
+func attack():
+	if till_next_attack > 0:
+		return false
+	till_next_attack = attack_timeout + attack_duration
+	is_attacking = true
+	return true
+	
 
 func _process(delta):
-	velocity = Vector2()
+	# Update attack delay
+	if till_next_attack > 0:
+		till_next_attack -= delta
+	else:
+		is_attacking = false
+	
+	# get user input
+	velocity *= 0
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -21,16 +46,21 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+	if Input.is_action_just_pressed("ui_select"):
+		attack()
+	
+	var next_speed = SPEED
+	if till_next_attack - attack_timeout > 0:
+		next_speed *= 10
 	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
+		velocity = velocity.normalized()
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.animation = "default"
 		$AnimatedSprite.stop()
 	
-	position += velocity * delta
-	
+	# Set animation
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right" # "left" for new sprites
 		$AnimatedSprite.flip_v = false
@@ -39,8 +69,12 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 	elif velocity.y > 0:
 		$AnimatedSprite.animation = "down"
-
-
+	
+	# Apply force
+	# position += velocity * delta
+	applied_force *= 0
+	add_force(empty_Vec2, velocity.normalized())
+	applied_force = applied_force.normalized() * next_speed * delta
 
 func _on_Player_body_entered( body ):
 	emit_signal("hit")
