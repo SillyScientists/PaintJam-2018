17708b1c04b21710b2bb3155a2436669275abc5e
Tim2462 <xdxderich@gmail.com>
2018-02-24 23:28:07 +0100

    Testing with Bodies

--- scenes/world/env.gd
+++ scenes/world/env.gd
@@ -4,6 +4,8 @@ var map = []
 
 var obstacles = [] #type,x,y
 
+var player_went_through_door = false
+
 var collision_map = []
 
 var current_map_name = "default.txt"
@@ -11,10 +13,11 @@ var current_map_name = "default.txt"
 var texture_map = {}
 var texture = load("res://icon.png")#Test
 
-var tile_size = 64
+export var tile_size = 64
 
 func load_textures():
-	var texture_files = [["0","scenes/textures/wall_vault/Kaum_Verfallener_Stein1"],
+	var texture_files = []
+	texture_files = [["0","scenes/textures/wall_vault/Kaum_Verfallener_Stein1"],
 						 ["1","scenes/textures/wall_vault/Kaum_Verfallener_Stein2"],
 						 ["2","scenes/textures/wall_vault/Sehr_Verfallener_Stein1"],
 						 ["3","scenes/textures/wall_vault/Sehr_Verfallener_Stein2"],
@@ -26,7 +29,7 @@ func load_textures():
 						 ["D","scenes/textures/ground/Verfallene_Bodenplatte_mit_Mos2"],
 						 ["E","scenes/textures/ground/Verfallene_Bodenplatte1"],
 						 ["F","scenes/textures/ground/Verfallene_Bodenplatte2"],
-						 ["#","res://icon"],
+						 ["#","scenes/textures/misc/Kaum_Verfallen_Bodenplatte_Mit_Tuer"],
 						 ["icon","res://icon"]]
 	texture_map = {}
 	for i in range(texture_files.size()):
@@ -34,14 +37,20 @@ func load_textures():
 
 func change_map(map_name):
 	current_map_name = map_name
-	print(current_map_name)
+	map = []
+	obstacles = []
+	print(current_map_name) 
 	map = create_map(load_file(current_map_name))
+	load_textures()
+	$".".update()
 
 
 func load_file(file_name):
 	var file = File.new()
 	file.open("worlds/" + file_name, file.READ)
 	var content = file.get_as_text()
+	#print("FILE : " + file_name)
+	#print("CONTENT : " + content)
 	file.close()
 	return content
 	
@@ -58,7 +67,7 @@ func create_map(input):
 	
 	#OBSTACLES
 	var obst = t1[2].split("#",true)
-	print(obst)
+	#print(obst)
 	for i in range(obst.size()):
 		obstacles.append([obst[i].split(",",true)[0],obst[i].split(",",true)[1],obst[i].split(",",true)[2]])
 	
@@ -68,28 +77,61 @@ func create_map(input):
 	var counter_line = 0
 	for i in n_map[0]:
 		if i=="0" or i=="1" or i=="2" or i=="3" or i=="4" or i=="5":
-			var coll = CollisionPolygon2D.new()
-			coll.polygon.append(Vector2(counter*tile_size,counter_line*tile_size))
-			coll.polygon.append(Vector2(counter*tile_size+tile_size,counter_line*tile_size))
-			coll.polygon.append(Vector2(counter*tile_size+tile_size,counter_line*tile_size+tile_size))
-			coll.polygon.append(Vector2(counter*tile_size,counter_line*tile_size+tile_size))
+			var phy_bod = RigidBody2D.new()
+			var coll = CollisionShape2D.new()
+			#coll.polygon.append(Vector2(counter*tile_size,counter_line*tile_size))
+			#coll.polygon.append(Vector2(counter*tile_size+tile_size,counter_line*tile_size))
+			#coll.polygon.append(Vector2(counter*tile_size+tile_size,counter_line*tile_size+tile_size))
+			#coll.polygon.append(Vector2(counter*tile_size,counter_line*tile_size+tile_size))
+			#coll.position = Vector2(counter*tile_size,counter_line*tile_size)
+			coll.shape = RectangleShape2D.new()
+			coll.shape.set_extents(Vector2(tile_size, tile_size))
 			
-			collision_map.append(coll)
+#			coll.polygon.append(Vector2(0*tile_size,0*tile_size))
+#			coll.polygon.append(Vector2(1*tile_size,0*tile_size))
+#			coll.polygon.append(Vector2(0*tile_size,1*tile_size))
+#			coll.polygon.append(Vector2(1*tile_size,1*tile_size))
+			
+			phy_bod.set_mode(1)
+			phy_bod.gravity_scale = 0
+			phy_bod.position = Vector2(counter*tile_size,counter_line*tile_size)
+			
+			#coll.scale = Vector2(64,64)
+			
+			phy_bod.add_child(coll)
+			phy_bod.set_physics_process(true)
+			
+			collision_map.append(phy_bod)
 		counter = counter + 1
 		if i == "\n":
 			counter_line = counter_line + 1
 			counter = 0
 	#COLLISIONS (OBSTACLES)
 	for i in range(obst.size()):
-		var coll = CollisionPolygon2D.new()
+		
+		#var collOb = Area2D.new()
+		var phy_bod = RigidBody2D.new()
+		var coll = CollisionShape2D.new()
 		var x = int(obst[i][1])
 		var y = int(obst[i][2])
-		coll.polygon.append(Vector2(x*tile_size,y*tile_size))
-		coll.polygon.append(Vector2(x*tile_size+tile_size,y*tile_size))
-		coll.polygon.append(Vector2(x*tile_size+tile_size,y*tile_size+tile_size))
-		coll.polygon.append(Vector2(x*tile_size,y*tile_size+tile_size))
 		
-		collision_map.append(coll)
+		#initializing shape
+		coll.shape = RectangleShape2D.new()
+		coll.shape.set_extents(Vector2(tile_size, tile_size))
+		
+		#initializing Rigid Body
+		phy_bod.set_mode(1)
+		phy_bod.gravity_scale = 0
+		phy_bod.position = Vector2(x*tile_size,y*tile_size)
+#		coll.polygon.append(Vector2(x*tile_size,y*tile_size))
+#		coll.polygon.append(Vector2(x*tile_size+tile_size,y*tile_size))
+#		coll.polygon.append(Vector2(x*tile_size+tile_size,y*tile_size+tile_size))
+#		coll.polygon.append(Vector2(x*tile_size,y*tile_size+tile_size))
+		
+		phy_bod.add_child(coll)
+		phy_bod.set_physics_process(true)
+		
+		collision_map.append(phy_bod)
 	
 	for i in collision_map: #Adding every collision object to the scene
 		add_child(i)
@@ -107,10 +149,40 @@ func _ready():
 	
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	
+	for i in collision_map:
+		i.update()
+	
+	var player_pos_x = int(get_node("Player").position[0]/64)
+	var player_pos_y = int(get_node("Player").position[1]/64)
+	
+	var counter = 0
+	var counter_line = 0
+	var door_count = -1
+	for i in map[0]:
+		if i == "#" :
+			door_count = door_count + 1
+		
+		if counter == player_pos_x and counter_line == player_pos_y:
+			if i == "#":
+				if not player_went_through_door:
+					var next_map = map[1][door_count][0]
+					get_node("Player").position[0] = int(map[1][door_count][1])*tile_size+(tile_size*0.5)
+					get_node("Player").position[1] = int(map[1][door_count][2])*tile_size+(tile_size*0.5)
+					change_map(next_map)
+					player_went_through_door = true
+			else :
+				player_went_through_door = false
+		if i == "\n":
+			counter_line = counter_line + 1
+			counter = 0
+		else:
+			counter = counter + 1
+	
+	pass
 
 
 func _draw():
@@ -126,6 +198,11 @@ func _draw():
 			counter = 0
 	for i in range(obstacles.size()):
 		draw_texture_rect(texture_map[obstacles[i][0]],Rect2(Vector2(int(obstacles[i][1])*tile_size,int(obstacles[i][2])*tile_size),Vector2(tile_size,tile_size)),false)
+	
+	for i in collision_map:
+		draw_rect(Rect2(i.position[0],i.position[1],64,64),Color(1,0,0),false)
+		#print(i.position)
+
 
 func get_at_pos(posx,posy):
 	var map_x = int((posx/tile_size)+1)
@@ -154,6 +231,6 @@ func get_at_pos(posx,posy):
 func _input(ev):
 	if ev.is_action_pressed("ui_select") :
 		print("space pressed")
-		change_map("default.txt")
+		change_map("test.txt")
 	if ev is InputEventMouseButton:
 		print(get_at_pos(ev.position.x,ev.position.y))
