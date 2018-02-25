97d34bfdfc6142822f4eb3032f4e2d8182164e86
Dommicrafter <dominikschumann1@gmail.com>
2018-02-25 15:26:56 +0100

    Adding GUI + evntually implemented to player

---
+++ scenes/GUI/zombie_count.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var zombies  
+
+func _ready():
+	zombies = get_tree().get_nodes_in_group("Zombie")
+	change_zom()
+	set_process(true)
+	
+
+func _process(delta):
+	if(zombies.size() != get_tree().get_nodes_in_group("Zombie").size()):
+		zombies = get_tree().get_nodes_in_group("Zombie")
+		chenge_zom()
+	
+
+func change_zom():
+	$zombie_text.text = zombies.size()
