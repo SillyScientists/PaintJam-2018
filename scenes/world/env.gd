7d74595a1a1307b97479980efce23693407dc047
Tim2462 <xdxderich@gmail.com>
2018-02-25 12:36:25 +0100

    Improving Doors + Adding Player Foot Position

--- scenes/world/env.gd
+++ scenes/world/env.gd
@@ -111,7 +111,23 @@ func create_map(input):
 			phy_bod.set_physics_process(true)
 			
 			collision_map.append(phy_bod)
+		
+		if i == "#":
+			var phy_bod = RigidBody2D.new()
+			var coll = CollisionShape2D.new()
+			coll.shape = RectangleShape2D.new()
+			coll.shape.set_extents(Vector2(tile_size/2,tile_size/6))
+			phy_bod.set_mode(1)
+			phy_bod.gravity_scale = 0
+			phy_bod.position = Vector2((counter)*tile_size+(tile_size/2),(counter_line)*tile_size+(tile_size/2))
+			
+			phy_bod.add_child(coll)
+			phy_bod.set_physics_process(true)
+			
+			collision_map.append(phy_bod)
+		
 		counter = counter + 1
+		
 		if i == "\n":
 			counter_line = counter_line + 1
 			counter = 0
@@ -147,80 +163,14 @@ func create_map(input):
 		phy_bod.set_physics_process(true)
 		
 		collision_map.append(phy_bod)
-		print("---------------------------")
-		print("X,Y = " + str(x) + " , " + str(y))
-		print("1 Obstacle wurde platziert bei : " + str(phy_bod.position))
-	
+		#print("---------------------------")
+		#print("X,Y = " + str(x) + " , " + str(y))
+		#print("1 Obstacle wurde platziert bei : " + str(phy_bod.position))
+		
 	for i in collision_map: #Adding every collision object to the scene
 		add_child(i)
 	
 	return n_map
-
-
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	
-	map = create_map(load_file(current_map_name))
-	load_textures()
-	
-	pass
-
-func _process(delta):
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
-	
-	for i in collision_map:
-		i.update()
-	
-	var player_pos_x = int(get_node("Player").position[0]/tile_size)
-	var player_pos_y = int(get_node("Player").position[1]/tile_size)
-	
-	var counter = 0
-	var counter_line = 0
-	var door_count = -1
-	for i in map[0]:
-		if i == "#" :
-			door_count = door_count + 1
-		
-		if counter == player_pos_x and counter_line == player_pos_y:
-			if i == "#":
-				if not player_went_through_door:
-					var next_map = map[1][door_count][0]
-					get_node("Player").global_position.x = int(map[1][door_count][1])*tile_size+(tile_size*0.5)
-					get_node("Player").global_position.y = int(map[1][door_count][2])*tile_size+(tile_size*0.5)
-					change_map(next_map)
-					player_went_through_door = true
-			else :
-				player_went_through_door = false
-		if i == "\n":
-			counter_line = counter_line + 1
-			counter = 0
-		else:
-			counter = counter + 1
-	
-	pass
-
-
-func _draw():
-	var counter = 0
-	var counter_line = 0
-	for i in map[0]: #MAP
-		if not i == "\n":
-			#draw_rect(Rect2(Vector2(counter*64,counter_line*64),Vector2(64,64)),Color(color_map[i]),true)
-			draw_texture_rect(texture_map[i],Rect2(Vector2(counter*tile_size,counter_line*tile_size),Vector2(tile_size,tile_size)),false)
-			counter = counter + 1
-		if i == "\n":
-			counter_line = counter_line + 1
-			counter = 0
-	for i in range(obstacles.size()):
-		draw_texture_rect(texture_map[obstacles[i][0]],Rect2(Vector2(int(obstacles[i][1])*tile_size,int(obstacles[i][2])*tile_size),Vector2(tile_size,tile_size)),false)
-	
-	#for i in collision_map:
-		#draw_rect(Rect2(i.position[0],i.position[1],64,64),Color(1,0,0),false)
-		#print(i.position)
-
 
 func get_at_pos(posx,posy):
 	var map_x = int((posx/tile_size)+1)
@@ -246,6 +196,114 @@ func get_at_pos(posx,posy):
 	
 	return [map_tile, obst]
 		
+func get_door(posx,posy):
+	var map_x = int((posx/tile_size)+1)
+	var map_y = int((posy/tile_size))
+	
+	var counter = 0
+	var counter_line = 0
+	var door_counter = -1
+	
+	for i in map[0]:
+		if i== "\n":
+			counter_line = counter_line + 1
+			counter = 0
+		else:
+			counter = counter + 1
+		
+		if i == "#" :
+			door_counter = door_counter + 1
+		
+		if map_x == counter and map_y == counter_line :
+			return map[1][door_counter]
+	pass
+	
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	map = create_map(load_file(current_map_name))
+	load_textures()
+	
+	pass
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	
+	for i in collision_map:
+		i.update()
+	
+	var player_pos_x = int(get_node("Player").get_node("FootPosition").global_position.x)
+	var player_pos_y = int(get_node("Player").get_node("FootPosition").global_position.y)
+	
+#	var counter = 0
+#	var counter_line = 0
+#	var door_count = -1
+#	for i in map[0]:
+#		if i == "#" :
+#			door_count = door_count + 1
+#
+#		var distance = sqrt(((player_pos_x-(counter+(tile_size/2)))*(player_pos_x-(counter+(tile_size/2))))+((player_pos_y-(counter_line+(tile_size/2)))*(player_pos_y-(counter_line+(tile_size/2)))))
+#
+#
+#		if distance <= 0.8:
+#			if i == "#":
+#				print(str(distance))
+#				print("OMG")
+#				if not player_went_through_door:
+#					var next_map = map[1][door_count][0]
+#					get_node("Player").global_position.x = float(map[1][door_count][1])*tile_size+(tile_size*0.5)
+#					get_node("Player").global_position.y = float(map[1][door_count][2])*tile_size+(tile_size*0.5)
+#					change_map(next_map)
+#					player_went_through_door = true
+#			else :
+#				player_went_through_door = false
+#		if i == "\n":
+#			counter_line = counter_line + 1
+#			counter = 0
+#		else:
+#			counter = counter + 1
+#
+	#print(str(player_pos_x) + " . " + str(player_pos_y))
+	#print(get_at_pos(round(player_pos_x),round(player_pos_y))[0])
+	if get_at_pos(round(player_pos_x),round(player_pos_y))[0] == "#":
+		if not player_went_through_door:
+			var door = get_door(player_pos_x,player_pos_y)
+			var next_map = door[0]
+			get_node("Player").global_position.x = float(door[1])*tile_size+(tile_size/2)
+			get_node("Player").global_position.y = float(door[2])*tile_size+(tile_size/2)
+			change_map(next_map)
+			player_went_through_door = true
+	else:
+		player_went_through_door = false
+	
+	pass
+
+
+func _draw():
+	var counter = 0
+	var counter_line = 0
+	for i in map[0]: #MAP
+		if not i == "\n":
+			#draw_rect(Rect2(Vector2(counter*64,counter_line*64),Vector2(64,64)),Color(color_map[i]),true)
+			draw_texture_rect(texture_map[i],Rect2(Vector2(counter*tile_size,counter_line*tile_size),Vector2(tile_size,tile_size)),false)
+			counter = counter + 1
+		if i == "\n":
+			counter_line = counter_line + 1
+			counter = 0
+	for i in range(obstacles.size()):
+		draw_texture_rect(texture_map[obstacles[i][0]],Rect2(Vector2(int(obstacles[i][1])*tile_size,int(obstacles[i][2])*tile_size),Vector2(tile_size,tile_size)),false)
+	
+	#for i in collision_map:
+		#draw_rect(Rect2(i.position[0],i.position[1],64,64),Color(1,0,0),false)
+		#print(i.position)
+
+
+
 func _input(ev):
 	if ev is InputEventMouseButton:
 		print(get_at_pos(ev.position.x,ev.position.y))
+		print(ev.position.x,ev.position.y)
